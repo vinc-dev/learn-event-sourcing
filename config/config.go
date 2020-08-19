9487db5da67f70fa2e635d7ae668e44718a325fd
Vincentius Devin Tandra <vincentius@payfazz.com>
2020-08-20 02:44:18 +0700

    fix: missing dialect import

--- config/config.go
+++ config/config.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -13,7 +14,7 @@ const (
 	ConfCORSEnabled = "true"
 
 	DB_USER = "postgres"
-	DB_PASS = "postgres"
+	DB_PASS = "cashfazz"
 	DB_NAME = "learn-event-sourcing"
 	DB_HOST = "localhost"
 	DB_PORT = "5432"
@@ -21,6 +22,10 @@ const (
 
 var db *gorm.DB
 var dbOnce sync.Once
+
+type gormConfigKey struct{}
+
+var GormConfigKey gormConfigKey
 
 func GetDB() *gorm.DB {
 	dbOnce.Do(func() {
