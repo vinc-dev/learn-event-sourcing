0f8517c267f0bb8fc37b6e96dfbfe56a0330d892
Vincentius Devin Tandra <vincentius@payfazz.com>
2020-08-20 01:06:56 +0700

    feature: adding GetDB func

--- config/config.go
+++ config/config.go
@@ -1,7 +1,47 @@
 package config
 
+import (
+	"fmt"
+	"sync"
+
+	"github.com/jmoiron/sqlx"
+)
+
 const (
 	ConfServerPort  = "3000"
 	ConfBaseURL     = "server.base_url"
 	ConfCORSEnabled = "true"
+
+	DB_USER = "postgres"
+	DB_PASS = "postgres"
+	DB_NAME = "learn-event-sourcing"
+	DB_HOST = "localhost"
+	DB_PORT = "5432"
 )
+
+var db *sqlx.DB
+var dbOnce sync.Once
+
+func GetDB() *sqlx.DB {
+	dbOnce.Do(func() {
+		var err error
+		var conn string
+		conn = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			DB_HOST,
+			DB_PORT,
+			DB_USER,
+			DB_PASS,
+			DB_NAME,
+		)
+
+		db, err := sqlx.Connect("postgres", conn)
+		if nil != err {
+			panic(err)
+		}
+
+		db.SetMaxOpenConns(1024)
+		db.SetMaxIdleConns(512)
+	})
+	return db
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/vinc-dev/learn-event-sourcing
 go 1.13
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.4
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
 	github.com/spf13/cast v1.3.1
 	github.com/stretchr/testify v1.4.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
